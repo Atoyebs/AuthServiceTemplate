c8a436a65bd4550788603fb8595a122a368695c2
Ini Atoyebi <iatoyebi.3134@gmail.com>
2024-07-07 19:50:26 +0100

    added default values and attached to submit button

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,21 +7,34 @@ const TUTORIAL_URL =
 
 export const SignUp = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [fistname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [email, setEmail] = useState("");
+  const [firstname, setFirstname] = useState("Ini");
+  const [lastname, setLastname] = useState("Atoyebi");
+  const [email, setEmail] = useState("bakid11888@atebin.com");
   const [password, setPassword] = useState("");
+
+  const onSignUp = async () => {
+    setIsLoading(true);
+    const body = {
+      firstname,
+      lastname,
+      email,
+      password,
+    }
+    alert(JSON.stringify(body))
+    setIsLoading(false);
+  };
 
   return (
     <section className="flex flex-col justify-center items-center h-full">
       <h1 style={{ marginBottom: "3rem" }}>Sign Up (Demo) </h1>
 
       <div style={{ gap: "1rem", width: "25vw" }} className="flex-col justify-center items-center">
-        <input placeholder="firstname" onChange={(e) => setFirstname(e.target.value)} />
-        <input placeholder="lastname" onChange={(e) => setLastname(e.target.value)} />
-        <input placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+        <input placeholder="firstname" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
+        <input placeholder="lastname" value={lastname} onChange={(e) => setLastname(e.target.value)} />
+        <input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         <input
           placeholder="password"
+          value={password}
           type="password"
           onChange={(e) => setPassword(e.target.value)}
         />
@@ -31,7 +44,7 @@ export const SignUp = () => {
         style={{ marginTop: "3rem", marginBottom: "1rem" }}
         className="flex-col justify-center items-center"
       >
-        <button disabled={isLoading} style={{ width: "30vw" }}>
+        <button onClick={onSignUp} disabled={isLoading} style={{ width: "30vw" }}>
           SignUp
         </button>
       </div>
