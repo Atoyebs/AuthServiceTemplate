57912a6aaf95553cbb2e78d95fffea359429e495
Ini Atoyebi <iatoyebi.3134@gmail.com>
2024-07-20 16:23:06 +0100

    created endpoint that handles verification from another microservice

--- app/config/appInfo.ts
+++ app/config/appInfo.ts
@@ -2,6 +2,6 @@ export const appInfo = {
   appName: "Template App",
   apiDomain: process.env.NEXT_SERVER_API_DOMAIN!,
   websiteDomain: process.env.NEXT_SERVER_WEBSITE_DOMAIN!,
-  apiBasePath: process.env.NEXT_SERVER_API_BASE_PATH!,
+  apiBasePath: process.env.NEXT_SERVER_SUPERTOKENS_API_BASE_PATH!,
   websiteBasePath: process.env.NEXT_SERVER_WEBSITE_BASE_PATH!,
 };
